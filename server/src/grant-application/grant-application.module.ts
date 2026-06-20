ad1e17b5f3bc9622e2202b58a736b76e3b5eadc2
shukrullo <boburovdev@gmail.com>
2026-06-20 13:06:16 +0500

    chore: remove server files and configurations; clean up unused code and dependencies

--- server/src/grant-application/grant-application.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { GrantApplicationController } from './grant-application.controller'
-import { GrantApplicationService } from './grant-application.service'
-
-@Module({
-  controllers: [GrantApplicationController],
-  providers: [GrantApplicationService],
-})
-export class GrantApplicationModule {}
